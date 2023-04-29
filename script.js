f2cdeb6b94a2a8dc822c10f17e66cc0dcf3a2cf5
sdooodly <gayathrigs96@gmail.com>
2023-04-30 00:27:50 +0530

    Base

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const container = document.getElementById("container");
 const slider = document.getElementById("slider");
+const container = document.querySelector('.container');
 
 slider.addEventListener("input", function() {
-  const numSquares = this.value * this.value;
-  container.innerHTML = "";
-  for (let i = 0; i < numSquares; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
+  const selectedValue = slider.value;
+  const squareRoot = Math.sqrt(selectedValue);
+  const squaresPerRow = Math.floor(squareRoot);
+  const squaresPerColumn = Math.ceil(squareRoot);
+
+  // Clear the container before adding new squares
+  container.innerHTML = '';
+
+  // Calculate the size of each square
+  const squareWidth = 500 / squaresPerRow;
+  const squareHeight = 500 / squaresPerColumn;
+
+  // Add new squares to the container
+  for (let i = 0; i < selectedValue; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `${squareWidth}px`;
+    square.style.height = `${squareHeight}px`;
     container.appendChild(square);
   }
 });
