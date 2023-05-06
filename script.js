921b5a308647ec9dbb7c92489d370440f98870a4
sdooodly <gayathrigs96@gmail.com>
2023-05-06 23:07:18 +0530

    Hm, this isn't working

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const coolButton = document.getElementById("cool-btn");
   const noirButton = document.getElementById("noir-btn");
 
+
   const warmColors = [
     "#FF5733",
     "#FF8C00",
@@ -90,77 +91,83 @@ document.addEventListener("DOMContentLoaded", function () {
     "#2C3E50",
     "#2E4053",
   ];
-
-  function getRandomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)];
-  }
-
-  warmButton.addEventListener("click", function () {
-    const selectedColor = getRandomColor(warmColors);
-    colorPicker.value = selectedColor;
-    document.body.style.backgroundColor = selectedColor;
-  });
-
-  coolButton.addEventListener("click", function () {
-    const selectedColor = getRandomColor(coolColors);
-    colorPicker.value = selectedColor;
-    document.body.style.backgroundColor = selectedColor;
-  });
-
-  noirButton.addEventListener("click", function () {
-    const selectedColor = getRandomColor(noirColors);
-    colorPicker.value = selectedColor;
-    document.body.style.backgroundColor = selectedColor;
-  });
-
-  colorPicker.addEventListener("change", function () {
-    const selectedColor = colorPicker.value;
-    document.body.style.backgroundColor = selectedColor;
-  });
-
-  slider.addEventListener("input", function () {
-    const selectedValue = slider.value * slider.value;
-    const squareRoot = Math.sqrt(selectedValue);
-    const squaresPerRow = Math.floor(squareRoot);
-    const squaresPerColumn = Math.ceil(squareRoot);
-
-    // Clear the container before adding new squares
-    container.innerHTML = "";
-    coloredSquares = [];
-
-    // Calculate the size of each square
-    const squareWidth = 500 / squaresPerRow;
-    const squareHeight = 500 / squaresPerColumn;
-
-    // Add new squares to the container
-    for (let i = 0; i < selectedValue; i++) {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      square.style.width = `${squareWidth}px`;
-      square.style.height = `${squareHeight}px`;
-      container.appendChild(square);
-
-      // Add event listeners to each square
-      square.addEventListener("mousedown", () => {
-        isMouseDown = true;
-        square.style.backgroundColor = colorPicker.value;
-        coloredSquares.push(square);
-      });
-
-      square.addEventListener("mousemove", () => {
-        if (isMouseDown && !coloredSquares.includes(square)) {
-          square.style.backgroundColor = colorPicker.value;
-          coloredSquares.push(square);
-        }
-      });
-
-      square.addEventListener("mouseup", () => {
-        isMouseDown = false;
-      });
-
-      square.addEventListener("mouseleave", () => {
-        isMouseDown = false;
-      });
+ 
+  
+    function getRandomColor(colors) {
+      return colors[Math.floor(Math.random() * colors.length)];
     }
+  
+    warmButton.addEventListener("click", function () {
+      const selectedColor = getRandomColor(warmColors);
+      colorPicker.value = selectedColor;
+      document.body.style.backgroundColor = selectedColor;
+    });
+  
+    coolButton.addEventListener("click", function () {
+      const selectedColor = getRandomColor(coolColors);
+      colorPicker.value = selectedColor;
+      document.body.style.backgroundColor = selectedColor;
+    });
+  
+    noirButton.addEventListener("click", function () {
+      const selectedColor = getRandomColor(noirColors);
+      colorPicker.value = selectedColor;
+      document.body.style.backgroundColor = selectedColor;
+    });
+  
+    colorPicker.addEventListener("change", function () {
+      const selectedColor = colorPicker.value;
+      document.body.style.backgroundColor = selectedColor;
+    });
+  
+    slider.addEventListener("input", function () {
+      const selectedValue = slider.value * slider.value;
+      const squareRoot = Math.sqrt(selectedValue);
+      const squaresPerRow = Math.floor(squareRoot);
+      const squaresPerColumn = Math.ceil(squareRoot);
+  
+      // Clear the container before adding new squares
+      container.innerHTML = "";
+      coloredSquares = [];
+  
+      // Calculate the size of each square
+      const squareWidth = 500 / squaresPerRow;
+      const squareHeight = 500 / squaresPerColumn;
+  
+      // Add new squares to the container
+      for (let i = 0; i < selectedValue; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareWidth}px`;
+        square.style.height = `${squareHeight}px`;
+        container.appendChild(square);
+  
+        // Add event listeners to each square
+        square.addEventListener("mousedown", () => {
+          if (!document.getElementById("hover-checkbox").checked) {
+            square.style.backgroundColor = colorPicker.value;
+          }
+          isMouseDown = true;
+          coloredSquares.push(square);
+        });
+  
+        square.addEventListener("mousemove", () => {
+          if (isMouseDown && !coloredSquares.includes(square)) {
+            if (document.getElementById("hover-checkbox").checked) {
+              square.style.backgroundColor = colorPicker.value;
+            }
+            coloredSquares.push(square);
+          }
+        });
+  
+        square.addEventListener("mouseup", () => {
+          isMouseDown = false;
+        });
+  
+        square.addEventListener("mouseleave", () => {
+          isMouseDown = false;
+        });
+      }
+    });
   });
-});
+  
